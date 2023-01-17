807828b4cf9efd95ea239a7c32121b1d27f2c36f
kiyoonewton <kiyoonewton41@gmail.com>
2023-01-17 13:59:58 +0100

    Add .env to gitignore execute error handler in app.js

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+//middleware
+app.use(express.json());
+
+//universal routes
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("<h1>Store API</h1> <a href='api/v1/products'></a>");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+//product routes
+
+//error handling
+app.use(notFound);
+app.use(errorHandler);
+
+//listen
+const start = () => {
+  try {
+    app.listen(port, console.log(`App listening on port ${port}`));
+  } catch (error) {}
+};
+
+start();
