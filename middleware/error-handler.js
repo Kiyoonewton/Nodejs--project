9a2b21e7fb85a306c20d28c69fd16ce2b17bfdb5
kiyoonewton <kiyoonewton41@gmail.com>
2023-01-17 13:58:42 +0100

    Add error-handler middleware

---
+++ middleware/error-handler.js
@@ -0,0 +1,7 @@
+const errorHandler = async (err, req, res, next) => {
+  return res
+    .status(500)
+    .json({ msg: "something went wrong, please try again later" });
+};
+
+module.exports = errorHandler;
