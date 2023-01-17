3ee472bcf2736b2c66edfef2d9aa7c2e60cb6ea0
kiyoonewton <kiyoonewton41@gmail.com>
2023-01-17 18:15:02 +0100

    Pass in mongoDB access

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const app = express();
+require("dotenv").config();
+
 const notFound = require("./middleware/not-found");
 const errorHandler = require("./middleware/error-handler");
-const app = express();
-const port = process.env.PORT || 3000;
+const { default: mongoose } = require("mongoose");
+
+const port = 5000;
 
 //middleware
 app.use(express.json());
 
 //universal routes
 app.get("/", (req, res) => {
-  res.send("<h1>Store API</h1> <a href='api/v1/products'></a>");
+  res.send(
+    "<h1>Store API</h1> <a href='api/v1/products'>Navigate to Product</a>"
+  );
 });
 
 //product routes
@@ -19,10 +25,13 @@ app.use(notFound);
 app.use(errorHandler);
 
 //listen
-const start = () => {
+const start = async () => {
   try {
-    app.listen(port, console.log(`App listening on port ${port}`));
-  } catch (error) {}
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(port, console.log(`App listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
