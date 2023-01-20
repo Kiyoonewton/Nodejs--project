1f2634addd331ee13cab5a4577c735f2019020be
kiyoonewton <kiyoonewton41@gmail.com>
2023-01-20 13:03:23 +0100

    Add complex query search

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+require("express-async-errors");
 const productsRouter = require("./route/productsRouter");
 
 const notFound = require("./middleware/not-found");
--- controller/products.js
+++ controller/products.js
@@ -1,9 +1,25 @@
-const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "get all the product" });
-};
+const Product = require("../model/productSchema");
 
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "get all static products" });
+  // throw new Error
+  const products = await Product.find({});
+  res.status(200).json({ products, nbHit: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  const { featured, company } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHit: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
